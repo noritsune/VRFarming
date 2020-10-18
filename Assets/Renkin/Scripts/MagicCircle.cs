3db83f264a01ff42362aa09b0f07e8e8cb51544e
noritsune <bill.gatess4037@gmail.com>
2020-10-18 23:57:30 +0900

    Fix: 床の魔法陣が正しい位置に残るように修正

--- Assets/Renkin/Scripts/MagicCircle.cs
+++ Assets/Renkin/Scripts/MagicCircle.cs
@@ -6,15 +6,34 @@ public class MagicCircle : MonoBehaviour
 {
     [SerializeField]
     private float _lifeTime;
+    [SerializeField]
+    private bool _canMove;
+
+    private Transform _parent_def;
+    private Vector3 _pos_def;
+    private Quaternion _rot_def;
 
     private void OnEnable()
     {
         StartCoroutine(CoDisableMySelf());
+
+        if(_canMove) return;
+        
+        _pos_def = transform.localPosition;
+        _rot_def = transform.localRotation;
+        _parent_def = transform.parent;
+        transform.parent = null;
     }
 
     private IEnumerator CoDisableMySelf()
     {
         yield return new WaitForSeconds(_lifeTime);
         gameObject.SetActive(false);
+
+        if(_canMove) yield break;
+
+        transform.parent = _parent_def;
+        transform.localPosition = _pos_def;
+        transform.localRotation = _rot_def;
     }
 }
---
+++ Assets/Renkin/Scripts/PosRotSyncher.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class PosRotSyncher : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _targetTrans;
+    [SerializeField]
+    private bool synch_pos_x;
+    [SerializeField]
+    private bool synch_pos_y;
+    [SerializeField]
+    private bool synch_pos_z;
+    [SerializeField]
+    private bool synch_rot_x;
+    [SerializeField]
+    private bool synch_rot_y;
+    [SerializeField]
+    private bool synch_rot_z;
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        if(synch_pos_x) pos.x = _targetTrans.position.x;
+        if(synch_pos_y) pos.y = _targetTrans.position.y;
+        if(synch_pos_z) pos.z = _targetTrans.position.z;
+        transform.position = pos;
+
+        Vector3 rot = transform.rotation.eulerAngles;
+        if(synch_rot_x) rot.x = _targetTrans.rotation.eulerAngles.x;
+        if(synch_rot_y) rot.y = _targetTrans.rotation.eulerAngles.y;
+        if(synch_rot_z) rot.z = _targetTrans.rotation.eulerAngles.z;
+        transform.rotation = Quaternion.Euler(rot);
+    }
+}
