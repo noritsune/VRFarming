9e1fa87e14ecf1a4a14689b09c1eb34cffb9e3cb
noritsune <bill.gatess4037@gmail.com>
2020-10-18 19:00:33 +0900

    Add: 両手を合わせたことを検知する仕組みを追加

---
+++ Assets/Renkin/Scripts/Edward.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Edward : MonoBehaviour
+{
+    [SerializeField]
+    private OVRHand.Hand _handType;
+
+    private string _anotherHandTag;
+
+    private void Start()
+    {
+        _anotherHandTag = _handType.Equals(OVRHand.Hand.HandLeft) ? "RightHand" : "LeftHand";
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag.Equals(_anotherHandTag))
+        {
+            Debug.Log("両手を合わせた！");
+        }
+    }
+}
