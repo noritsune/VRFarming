545e3cf20992f1ba55f982c14232fa6ba300ae53
noritsune <bill.gatess4037@gmail.com>
2020-10-18 22:25:36 +0900

    Add: 地面に手をついて魔法陣を出す機能を追加

--- Assets/Renkin/Scripts/Edward.cs
+++ Assets/Renkin/Scripts/Edward.cs
@@ -7,6 +7,8 @@ public class Edward : MonoBehaviour
     private OVRHand.Hand _handType;
     [SerializeField]
     private List<GameObject> _magicCircles;
+    [SerializeField]
+    private GameObject _magicCircles_onGround;
 
     private string _anotherHandTag;
 
@@ -20,10 +22,20 @@ public class Edward : MonoBehaviour
         if(other.tag.Equals(_anotherHandTag))
         {
             Debug.Log("両手を合わせた！");
+
+            //手の甲に魔法陣を出す
             foreach (var magicCircle in _magicCircles)
             {
                 magicCircle.SetActive(true);
             }
+        }
+
+        if(other.tag.Equals("Floor"))
+        {
+            if(!_magicCircles[0].activeSelf) return;
+
+            //地面に魔法陣を出す
+            _magicCircles_onGround.SetActive(true);
         }
     }
 }
---
+++ Assets/Renkin/Scripts/KeepOnGround.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class KeepOnGround : MonoBehaviour
+{
+    [SerializeField]
+    private float offset_y;
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        pos.y = offset_y;
+        transform.position = pos;
+        
+        transform.rotation = Quaternion.identity;
+    }
+}
