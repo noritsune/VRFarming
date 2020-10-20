5007358fbc8354a266903e01fa158e0655afba8b
noritsune <bill.gatess4037@gmail.com>
2020-10-21 02:15:24 +0900

    Add: プレイヤーに向かって歩いてくるゾンビを追加

---
+++ Assets/Renkin/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    private Transform _playerTrans;
+    private NavMeshAgent _navMeshAgent;
+    private Animator _animator;
+
+    void Start()
+    {
+        _playerTrans = GameObject.Find("CenterEyeAnchor").transform;
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        _animator = GetComponent<Animator>();
+        _animator.SetFloat("MoveSpeed", _navMeshAgent.speed * 5);
+    }
+
+    void Update()
+    {
+        _navMeshAgent.destination = _playerTrans.position;
+    }
+}
